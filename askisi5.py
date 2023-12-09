fbba1a9bb6a7dddc7bfc27e47b54472311a3cd07
karyotakis <karyotakis@gmail.com>
2023-12-09 14:34:32 +0200

    Ολοκληρώθηκε

---
+++ askisi5.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Άσκηση 5
+a = input("Δώσε A επίδοση: ")
+b = input("Δώσε B επίδοση: ")
+c = input("Δώσε C επίδοση: ")
+
+mesi_timi = (a+b+c)/3.0
+print "Η μέση τιμή είναι ", mesi_timi
+
+if mesi_timi > 8:
+    print "Προκρίθηκες"
---
+++ askisi6.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Άσκηση 6
+# Χωρίς έλεγχο ορθότητας εισαγωγής αριθμών
+apoysies = input("Δώσε αριθμό συνολικών απουσιών μαθητή: ")
+dikaiologhmenes = input("Δώσε αριθμό δικαιολογημένων απουσιών από γιατρό: ")
+
+if dikaiologhmenes > 50:
+    dikaiologhmenes = 50
+
+if apoysies - dikaiologhmenes > 130:
+    print "Ο μαθητής απορρίπτεται λόγω απουσιών."
+else:
+    print "Ο μαθητής μπορεί να δώσει εξετάσεις."
